d1a96c16464b9df0b6ae60ed4c31c900669677fc
EditYJ <158392613@qq.com>
2020-10-23 15:49:24 +0800

    第 7 章 日志 异常捕获

--- src/com/edityj/v1ch03/InputTest.java
+++ src/com/edityj/v1ch03/InputTest.java
@@ -1,51 +1,34 @@
 package com.edityj.v1ch03;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Paths;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class InputTest {
   final static String THIS_PATH = System.getProperty("user.dir");
 
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-
-    // get first input
-//    System.out.println("你的名字？");
-//    String name = in.nextLine();
-
-    // get second input
-//    System.out.println("你的年龄？");
-//    int age = in.nextInt();
-
-    // display user info
-//    System.out.println("姓名：" + name + "\n" + "年龄：" + age);
-
-    // 读取和存取
+    final String path = THIS_PATH + "\\README.md";
     StringBuilder content = new StringBuilder();
-    try {
-      final String path = THIS_PATH + "\\README.md";
 
+    try (Scanner fileIn = new Scanner(Paths.get(path), "utf-8");) {
       // 读取原来数据
-      Scanner fileIn = new Scanner(Paths.get(path), "utf-8");
       while (fileIn.hasNext()) {
-        content.append(fileIn.nextLine()).append("\n");
+        final String nextLine = fileIn.nextLine();
+        content.append(nextLine).append("\n");
       }
-      fileIn.close();
-
       // 写入新数据
-      content.append("我是测试行3777");
+      content.append("我是测试行37123213");
       PrintWriter out = new PrintWriter(path, "utf-8");
       out.print(content);
+      Logger.getGlobal().info("File->" + content);
+//      System.out.println(content);
       out.close();
-
-      System.out.println(content);
     } catch (IOException e) {
-      System.out.println(e.toString());
+      e.printStackTrace();
     }
   }
 }
